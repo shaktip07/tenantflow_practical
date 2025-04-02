1aefb656ac77d6163c8ded23085d1f61ee302cef
shakti <shaktipatel750@gmail.com>
2025-04-02 12:45:20 +0530

    settings updated

--- settings.py
+++ settings.py
@@ -16,17 +16,12 @@ CORS_ALLOWED_ORIGINS = config(
     default="*",
 )
 
-# DB_INSTANCE_HOST = config("DB_INSTANCE_HOST", default="localhost")
-# DB_NAME = config("DB_NAME", default="organization_management_temp")
-# DB_USER = config("DB_USER", default="postgres")
-# DB_PASSWORD = config("DB_PASSWORD", default="Patel@1234")
-# DB_PORT = config("DB_PORT", default="5432")
+DB_INSTANCE_HOST = config("DB_INSTANCE_HOST", default="localhost")
+DB_NAME = config("DB_NAME", default="organization_management")
+DB_USER = config("DB_USER", default="postgres")
+DB_PASSWORD = config("DB_PASSWORD", default="Patel@1234")
+DB_PORT = config("DB_PORT", default="5432")
 
-DB_INSTANCE_HOST = "localhost"
-DB_NAME = "organization_management_temp"
-DB_USER = "postgres"
-DB_PASSWORD = "Patel1234"
-DB_PORT = "5432"
 
 DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_INSTANCE_HOST}:{DB_PORT}/{DB_NAME}"
 
